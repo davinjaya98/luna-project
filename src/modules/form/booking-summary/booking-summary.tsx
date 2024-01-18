ca5cf7936450e3de1ee97d2ebda2ee14bffed57e
ivanmartinn <ivanmartinn95@gmail.com>
2024-01-19 01:28:01 +0700

    booking summary initial

---
+++ src/modules/form/booking-summary/booking-summary.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Form, Button, Row, Col, Stack, Accordion } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+
+const BookingSummaryForm = () => {
+  return <></>;
+};
+
+export default BookingSummaryForm;

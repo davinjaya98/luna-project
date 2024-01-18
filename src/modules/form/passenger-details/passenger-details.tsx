94f476f457fd79f5966044582569e47a3428aff6
ivanmartinn <ivanmartinn95@gmail.com>
2024-01-19 00:45:54 +0700

    modal initiate

--- src/modules/form/passenger-details/passenger-details.tsx
+++ src/modules/form/passenger-details/passenger-details.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import { Form, Button, Row, Col, Stack, Accordion } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
 
 const PassengerDetailsForm = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  function handleSave() {
+    // MARK: -TODO Save before dismissing
+    setShow(false);
+  }
+
   return (
     <Stack gap={3}>
       <Accordion>
@@ -131,8 +142,26 @@ const PassengerDetailsForm = () => {
         </Accordion.Item>
       </Accordion>
       <div className="d-grid">
-        <Button variant="secondary">+ Add More Passenger</Button>
+        <Button variant="secondary" onClick={handleShow}>
+          + Add More Passenger
+        </Button>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header className="border-0" closeButton>
+          <Modal.Title>Save Pesenger details?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          New pesenger detected. Do you want to save this data?
+        </Modal.Body>
+        <Modal.Footer className="border-0">
+          <Button variant="outline-primary" onClick={handleClose}>
+            Maybe Next Time
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Stack>
   );
 };
